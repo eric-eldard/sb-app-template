50257d0ba74d254e100cd598269cd8a32d93f26e
Eric Eldard <eric.eldard@gmail.com>
2025-09-02 16:20:30 -0400

    fixes "cloc requires global install"

--- scripts/cloc.sh
+++ scripts/cloc.sh
@@ -1,10 +1,12 @@
-# https://www.npmjs.com/package/cloc / https://github.com/AlDanial/cloc
+# cloc is Perl software for counting file lines which can be installed and run from npm.
+# https://github.com/AlDanial/cloc
+# https://www.npmjs.com/package/cloc
 
 BOLD_WHITE="\e[1;97m"
 RESET="\e[0m"
 
 printf "\n\n${BOLD_WHITE}Lines of application code${RESET}\n\n"
-cloc --not-match-d="(node_modules|out|generated)" \
+npx cloc --not-match-d="(node_modules|out|generated)" \
  maintenance-page \
  scripts \
  sql \
@@ -12,4 +14,4 @@ cloc --not-match-d="(node_modules|out|generated)" \
  pom.xml
 
 printf "\n\n${BOLD_WHITE}Lines of application test code${RESET}\n\n"
-cloc src/test
+npx cloc src/test
